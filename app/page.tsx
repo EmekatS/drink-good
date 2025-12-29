d64c703b5ac7c805417c221f6d051464bbfdb8d1
3mekat5 <3mekat5@gmail.com>
2025-12-29 22:02:58 +0100

    Navbar

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <section className={"w-full flex-between mx-auto p-10"}>
+            <div className={"w-1/3 h-full"}>
+                <h1>Hello world</h1>
+            </div>
+            <div className={"w-2/3 h-full"}>Hello Mars</div>
+        </section>
+    )
+}
+export default Page
